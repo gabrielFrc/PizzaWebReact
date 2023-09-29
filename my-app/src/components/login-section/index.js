d936b9971ec0038fb97fcce29c251053bede880e
Gabriel França <70562629+gabrielFrc@users.noreply.github.com>
2023-09-29 11:10:58 -0300

    Added remember me checkbox and fixed bugs in account login

--- my-app/src/components/login-section/index.js
+++ my-app/src/components/login-section/index.js
@@ -2,24 +2,25 @@ import { useState } from 'react';
 import './index.css';
 
 const LoginSection = (props) => {
-    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
-    // const [setIsLogged] = useState();
+    const [password, setPassword] = useState('');
+    const [account, setAccount] = useState(JSON.parse(localStorage.getItem('account')))
 
-    let account;
-    if(localStorage.getItem('account') != null){
-        account = JSON.parse(localStorage.getItem('account'));
-    }else{
+    if(account == null){
         localStorage.setItem('account', JSON.stringify({logged: false}))
+        setAccount(JSON.parse(localStorage.getItem('account')));
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
         
-        if(username === 'John' && email === 'johndoe@email.com'){
-            localStorage.setItem('account', JSON.stringify({logged: true}))
+        if(password === 'John123' && email === 'johndoe@email.com'){
+            localStorage.setItem('account', JSON.stringify({logged: true}));
             // setIsLogged(true);
-            setUsername('');
+            // setUsername('');
+            // setEmail('');
+            // forceUpdate(update+1);
+            setAccount(JSON.parse(localStorage.getItem('account')));
         }
     }
 
@@ -34,16 +35,20 @@ const LoginSection = (props) => {
                 {!account.logged && <form className='input-label' onSubmit={handleSubmit}>
                     <h1>Welcome Back!</h1>
                     <label>
-                        <input type="text" name="name" placeholder='Name'
-                        value={username} onChange={(event) => {
-                            setUsername(event.target.value)
-                        }}/>
-                    </label>
-                    <label>
                         <input type="email" name="email" placeholder='Email'
                         value={email} onChange={(event) => {
                             setEmail(event.target.value)
                         }}/>
+                    </label>
+                    <label>
+                        <input type="text" name="name" placeholder='Password'
+                        value={password} onChange={(event) => {
+                            setPassword(event.target.value)
+                        }}/>
+                    </label>
+                    <input type="checkbox" value="" id="flexCheckDefault"/>
+                    <label className='label-checkbox'>
+                        Remember me
                     </label>
                     <label className='submit-button'>
                         <input type="submit" value="Log In"/>
@@ -55,7 +60,11 @@ const LoginSection = (props) => {
                         <h4>johndoe@email.com</h4>
                         <h2>John Doe</h2>
                     </div>
-                    <button id='logout-button'>Log out</button>
+                    <button id='logout-button' onClick={() => {
+                        localStorage.setItem('account', JSON.stringify({logged: false}));
+                        // setUsername(username + ' ');
+                        setAccount(JSON.parse(localStorage.getItem('account')));
+                    }}>Log out</button>
                 </div>}
             </div>
         </>
